10419b1770f776566099225d4d0766a3e7b29855
PrinceShekhawat01 <148666407+PrinceShekhawat01@users.noreply.github.com>
2024-12-08 11:14:56 +0000

    goot to learn about variables and dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,16 @@
+"use strict";
+//alert(3+3) ?
+//code readability should be high 
+//clean code ?   
+console.log(3)
+let name="prince"
+let age=18
+let isLoggedIn=false
+//boolean 
+//null=>standalone value 
+//undefined
+//symbol for the uniqueness  
+let state=null
+//object the real world enitry according to me
+console.log(typeof null)
+console.log("prince")

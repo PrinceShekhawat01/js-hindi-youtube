aa62b49ab24590ce092e12e970e7e57fc51a71f5
PrinceShekhawat01 <148666407+PrinceShekhawat01@users.noreply.github.com>
2024-12-10 10:49:02 +0000

    changes done

--- 01_basics/03_operation.js
+++ 01_basics/03_operation.js
@@ -1,23 +1,30 @@
-/*operations*/
-/*coding me sabse important hai readability*/
+//++++++++++++++++++++++++++++++++++++++++++++++++
+//Stack(Primitive) ,Heap(Non primitive)
+//is this possible to copy like the primitive but from the non pimitive and how 
 
-/*let value=5/2
-console.log(value)
-console.log(typeof(value))
-let str1="hitesh"
-let str2=" hitesh"
-let str3=str1+str2
-console.log(str3)
-console.log("1"+2)
-console.log("1"+2+3)
-console.log(1+2+"3")
-console.log(1+"2"+3)
-console.log("1"+2+"3")
-let a=+true*/
+/*let myYoutubename="hitestchaudhary.com";
+let anothername=myYoutubename
+anothername="chaiaurcode";
+console.log(myYoutubename);
+console.log(anothername);*/
 
+/*let userOne={
+    email:"user@google.com",
+    upi:"user@ybl"  //////////////doubt /////////////
+};
+let userTwo=userOne;
+console.log(userOne);
+userTwo={
+    email:"kamalprince01@gmail.com",
+    upi:"hello"
+};
+console.log(userOne);*/
 
-let num1,num2,num3
-num1=num2=num3=2+2
-let gameCounter=100;
-++gameCounter;
-console.log(gameCounter);
+let userOne={
+    email:"user@google.com",
+    upi:"user@ybl"  //////////////doubt /////////////
+};
+let userTwo=userOne;
+userTwo.email="kamalprince@gmail.com";
+console.log(userTwo);
+console.log(userOne);

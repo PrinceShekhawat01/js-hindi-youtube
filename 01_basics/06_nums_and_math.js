1a1a5ef1ca55a072e3c6cd6a58bc690e70913408
PrinceShekhawat01 <148666407+PrinceShekhawat01@users.noreply.github.com>
2024-12-14 06:40:00 +0000

    about numbers and math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,19 @@
+/*const score=411.99999;
+const balance=new Number(100);//explicit declaration of the number 
+console.log(typeof(balance.toPrecision(5)));
+console.log((balance.toString())+"Okay");
+console.log(balance.toFixed(1));
+const otherNumber=23.8966;
+console.log(otherNumber.toFixed(2));
+console.log(score.toPrecision(3));
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-IN'));*/
+//++++++++++++++++++++++ MATH +++++++++++++++++++++ 
+
+// let a=-1;
+// console.log(Math.abs(a));
+// console.log(typeof(Math.ceil(4.6)));
+//console.log(Math.random()*10+1);
+/*const min=10;
+const max=20;       ++++++doubt++++++ */
+//console.log(Math.floor((Math.random()*((max-min)+1)))+min);

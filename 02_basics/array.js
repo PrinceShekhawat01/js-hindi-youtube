0180e39f7f95667305ad525fc2b76e1033a7e985
PrinceShekhawat01 <148666407+PrinceShekhawat01@users.noreply.github.com>
2025-01-10 04:12:36 +0000

    arrays

---
+++ 02_basics/array.js
@@ -0,0 +1,12 @@
+//prototype super power hai javascript ki.
+//spread
+//concat 
+//push
+//flat 
+//console.log(Array.from({0:"prince",length:1}));     
+//from 
+//of 
+// const num1=100;
+// const num2=200;
+// const num3=300;
+// console.log(Array.of(num1,num2,num3));

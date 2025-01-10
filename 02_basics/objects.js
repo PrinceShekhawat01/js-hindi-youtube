f50251b0031d59a6639f238186cb44522a7edb4c
PrinceShekhawat01 <148666407+PrinceShekhawat01@users.noreply.github.com>
2025-01-10 09:56:07 +0000

    objects

---
+++ 02_basics/objects.js
@@ -0,0 +1,4 @@
+//object jo hai bo 2 parkar su banayo jaa sak hai ek to constructor ki help su or ek literal ki help su 
+//jo constructor ki help su banayo jaav hai bo hov hai singularity 
+//interview question important hai ki ek symbol liyo or bin object ki keys k roop m represent karo or baad m bin print karo 
+//javascript sikhni hai to 2 cheez sikhlyo khel katam hai ek hai objects or ek hai events

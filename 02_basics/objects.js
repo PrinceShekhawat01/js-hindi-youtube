fb86eef940b159b46cb7d62d5f75ef9a7c04ab38
PrinceShekhawat01 <148666407+PrinceShekhawat01@users.noreply.github.com>
2025-01-11 07:38:15 +0000

    json and objects

--- 02_basics/objects.js
+++ 02_basics/objects.js
@@ -14,21 +14,61 @@
 // tinderUser.name="Same";
 // tinderUser.isLoggedIn=false;
 // console.log(tinderUser);
-const regularUser={
-    email:"some@gmail.com",
-    fullname:{
-        userfullname:{
-            firstname:"hitesh",
-            lastname:"chaudhary"
-        }
-    }
-}
-//console.log(regularUser.fullname.userfullname.firstname);
-const obj1={
-    1:"a",
-    2:"b"
-};
-const obj2={3:"a",4:"b"};
-//const obj3={"obj1","obj2"};
-const obj3={...obj1,...obj2};
-console.log(obj3);
+// const regularUser={
+//     email:"some@gmail.com",
+//     fullname:{
+//         userfullname:{
+//             firstname:"hitesh",
+//             lastname:"chaudhary"
+//         }
+//     }
+// }
+// //console.log(regularUser.fullname.userfullname.firstname);
+// const obj1={
+//     1:"a",
+//     2:"b"
+// };
+// const obj2={3:"a",4:"b"};
+// //const obj3={"obj1","obj2"};
+// const obj3={...obj1,...obj2};
+// console.log(obj3);
+
+
+
+
+
+
+
+
+
+
+
+
+//    ///////////////////////////////                   11-Jan-2025                  //////////////////////////////
+
+// de structuring can be done for both arrays and objects 
+// const course={
+//     coursename:"js in hindi ",
+//     price:"999",
+//     courseteacher:"hitesh sir"
+// }
+// const {courseteacher:ct}=course;
+// console.log(ct);
+
+// const navvar=()=>{
+
+// }
+// navbar(company="hitesh")
+
+// const animal={
+//     speciesoftheanimal:"panthera",
+//     foodchainrank:3
+// };
+// const {speciesoftheanimal:soa}=animal;
+// console.log(soa);
+
+//apis ko kaayi seen hai o bhi dekhno padsi 
+// {
+//     //json and the object literal o hi hai 
+// }
+

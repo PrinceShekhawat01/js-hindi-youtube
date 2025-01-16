88e863dbba93941a1474d296b95c21f5bee9b342
PrinceShekhawat01 <148666407+PrinceShekhawat01@users.noreply.github.com>
2025-01-16 13:26:16 +0000

    good

--- 03_basics/functions.js
+++ 03_basics/functions.js
@@ -4,6 +4,9 @@
 // }
 // sayMyName; --->Reference
 // sayMyName(); ---->execution 
+//rest or spread operator 
+
+
 
 
 // function addTwoNumbers(number1,number2){
@@ -14,8 +17,35 @@
 // console.log(result);
 
 
-function loginUserMessage(username){
-    return `${username} just logged in`
+// function loginUserMessage(username){
+//     return `${username} just logged in`
    
-}
-console.log(loginUserMessage("prince"));
+// }
+// console.log(loginUserMessage("prince"));
+
+// function calculateCartPrice(val1,val2,...num1){
+//     return num1                  //business logic for a cart with 
+//                                 //multiple items selected 
+// }
+// console.log(calculateCartPrice(200,400,500,600,700))
+
+// const user={
+//     username:"hitesh",
+//     price:199
+// };
+// function handleObj(anyObj){
+//     console.log(`Username is ${anyObj.username} and price is ${anyObj.price}`);
+
+// }
+// handleObj({
+//     username:"sam",
+//     price:399
+// })
+//in the above example we passed the object directly for the solution of the problem if there is price changes  to prices in any case 
+
+// const mynewArr=[200,400,100,600]
+// function printvalue(arr){
+//     return arr[1];                     //passing array to the fun.
+// };
+// console.log((mynewArr));
+
